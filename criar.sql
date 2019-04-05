c9026549700ac720b48a29ab86231bb2f0b285b5
Unknown <anamargaridarl@gmail.com>
2019-04-05 18:36:27 +0100

    started adding tables to sql

---
+++ criar.sql
@@ -0,0 +1,127 @@
+PRAGMA foreign_keys = ON;
+BEGIN TRANSACTION; 
+
+DROP TABLE if exists Utente;
+
+CREATE TABLE Utente (
+nCC INTEGER ,
+nome CHAR,
+dataNascimento TEXT,
+morada CHAR,
+telefone INTEGER ,
+nSaude INT PRIMARY KEY,
+UNIQUE(nCC,telefone)
+/*/vagas*/
+);
+
+DROP TABLE if exists Funcionario;
+
+CREATE TABLE Funcionario (
+nCC INTEGER,
+nome CHAR,
+dataNascimento TEXT,
+morada CHAR,
+telefone INTEGER ,
+idHospital INT PRIMARY KEY,
+UNIQUE(nCC,telefone)
+/*/vagas*/
+);
+
+DROP TABLE if exists Medico;
+
+CREATE TABLE Medico (
+especialidade CHAR,
+idHospital REFERENCES Funcionario PRIMARY KEY
+);
+
+
+DROP TABLE if exists Enfermeiro;
+
+CREATE TABLE Enfermeiro (
+especialidade CHAR,
+idHospital REFERENCES Funcionario PRIMARY KEY
+);
+
+
+DROP TABLE if exists Data;
+
+CREATE TABLE Data (
+idHospital REFERENCES Funcionario,
+data TEXT,
+idAgenda REFERENCES Agenda,
+PRIMARY KEY(idHospital,data)
+);
+
+
+DROP TABLE if exists Agenda;
+
+CREATE TABLE Agenda (
+idAgenda INTEGER PRIMARY KEY,
+horaEntrada INTEGER,
+horaSaida INTEGER
+);
+
+
+DROP TABLE if exists UtenteAlergia;
+
+CREATE TABLE UtenteAlergia (
+nSaude REFERENCES Utente,
+substancia REFERENCES Alergia,
+/*classe de associaçao*/
+PRIMARY KEY(nSaude,substancia)
+);
+
+
+DROP TABLE if exists Alergia;
+
+CREATE TABLE Alergia (
+substancia TEXT PRIMARY KEY,
+descricao TEXT
+);
+
+
+DROP TABLE if exists Departamento;
+
+CREATE TABLE Departamento (
+idLocalizacao TEXT PRIMARY KEY,
+capacidade INTEGER,
+nome TEXT
+/*/vagas*/
+);
+
+
+DROP TABLE if exists Trabalha;
+
+CREATE TABLE Trabalha (
+idLocalizacao REFERENCES Departamento,
+idHospital REFERENCES Funcionario,
+PRIMARY KEY(idLocalizacao,idHospital)
+);
+
+
+DROP TABLE if exists ProcessoEnfermeiro;
+
+CREATE TABLE ProcessoEnfermeiro (
+idProcesso REFERENCES Processo,
+idHospital REFERENCES Funcionario,
+PRIMARY KEY(idProcesso,idHospital)
+/*classe de associacao*/
+);
+
+
+DROP TABLE if exists Processo;
+
+CREATE TABLE Processo (
+idProcesso INTEGER PRIMARY KEY,
+tipoProcesso TEXT,
+horaEntrada INTEGER,
+horaSaida INTEGER,
+nSaude REFERENCES Utente
+/*classe de associacao*/
+);
+
+COMMIT;
+
+
+
+
