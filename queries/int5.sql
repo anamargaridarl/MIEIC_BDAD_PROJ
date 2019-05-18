58a40e461b44689fd0e1fbdc2e076977235caeb6
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-05-19 00:25:45 +0100

    Made query number 5

---
+++ queries/int5.sql
@@ -0,0 +1,11 @@
+SELECT idHospital, nome, numProc
+FROM (SELECT idHospital, count(idProcesso) AS numProc
+    FROM Medico LEFT JOIN EncarregueMed USING(idHospital)
+    GROUP BY idHospital
+
+    UNION
+
+    SELECT idHospital, count(idProcesso) AS numProc
+    FROM Enfermeiro LEFT JOIN EncarregueEnf USING(idHospital)
+    GROUP BY idHospital) NATURAL JOIN Funcionario NATURAL JOIN Pessoa
+ORDER BY numProc DESC;
