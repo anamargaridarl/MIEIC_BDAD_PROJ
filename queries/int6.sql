78926decebb60938c0036556a32c4c147557616b
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-05-21 15:05:42 +0100

    Started ints 6 and 9

--- queries/int6.sql
+++ queries/int6.sql
@@ -1,3 +1,5 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+-- Quais os medicamentos que cada utente internado (cujo processo ainda se encontra ativo) toma?
--- queries/int9.sql
+++ queries/int9.sql
@@ -1,3 +1,5 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+-- Quais os médicos disponíveis (i.e. que não se encontram encarregues de nenhum processo) num certo dia e hora?
