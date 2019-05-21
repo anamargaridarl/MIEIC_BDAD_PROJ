2cc3385733f18489b0528035a013fd2e902d704c
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-05-21 16:00:14 +0100

    Added 2nd trigger and every needed modification on criar and povoar. Small change on 3rd query

--- queries/int3.sql
+++ queries/int3.sql
@@ -3,15 +3,15 @@
 .nullvalue NULL
 
 -- Quais os funcionários envolvidos num determinado processo?
-select idProcesso, nome from Pessoa,
-    (select idProcesso, nCC 
+select idProcesso, idHospital, nome from Pessoa,
+    (select idProcesso, F.idHospital, nCC 
     from Funcionario, 
         (select distinct Processo.idProcesso, Processo.tipoProcesso, EncarregueEnf.idHospital
-        from Processo, EncarregueEnf, EncarregueMed 
+        from Processo, EncarregueEnf 
         on Processo.idProcesso=EncarregueEnf.idProcesso
         union
         select distinct Processo.idProcesso, Processo.tipoProcesso, EncarregueMed.idHospital
         from Processo, EncarregueMed
-    on Processo.idProcesso=EncarregueMed.idProcesso) as F 
+        on Processo.idProcesso=EncarregueMed.idProcesso) as F 
     on F.idHospital=Funcionario.idHospital) as P 
 on P.nCC=Pessoa.nCC order by idProcesso asc;
---
+++ triggers/gatilho2_verifica.sql
@@ -0,0 +1,18 @@
+INSERT INTO Departamento values(8, 5, 'Urgencia', 5);
+
+SELECT * FROM Departamento WHERE idDepartamento=8;
+INSERT INTO ProcessoDepartamento values(1,8);
+SELECT * FROM Departamento WHERE idDepartamento=8;
+INSERT INTO ProcessoDepartamento values(2,8);
+SELECT * FROM Departamento WHERE idDepartamento=8;
+INSERT INTO ProcessoDepartamento values(3,8);
+SELECT * FROM Departamento WHERE idDepartamento=8;
+INSERT INTO ProcessoDepartamento values(4,8);
+SELECT * FROM Departamento WHERE idDepartamento=8;
+INSERT INTO ProcessoDepartamento values(5,8);
+SELECT * FROM Departamento WHERE idDepartamento=8;
+INSERT INTO ProcessoDepartamento values(7,8);
+SELECT * FROM Departamento WHERE idDepartamento=8;
+SELECT * FROM ProcessoDepartamento WHERE idDepartamento=8;
+
+DELETE FROM Departamento WHERE idDepartamento = 8;
