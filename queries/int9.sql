94499220f17a02f09d9afb621b65039c3306bd2f
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-05-23 11:33:10 +0100

    Finished query 9

--- queries/int9.sql
+++ queries/int9.sql
@@ -3,9 +3,11 @@
 .nullvalue NULL
 
 -- Quais os médicos disponíveis (i.e. que não se encontram encarregues de nenhum processo) num certo dia e hora?
-select * 
-from Agenda, 
+
+select Pessoa.*
+from Pessoa, Funcionario, Agenda, 
     (select idHospital, idAgenda 
     from Data 
-    where idHospital not in (select idHospital from EncarregueMed) and data = '2019-04-15') as agendas
-on Agenda.idAgenda=agendas.idAgenda;
+    where idHospital not in (select idHospital from EncarregueMed) and data='2019-04-15') as agendas
+on Agenda.idAgenda=agendas.idAgenda and Funcionario.idHospital=agendas.idHospital and Pessoa.nCC=Funcionario.nCC
+where horaSaida > '16:00';
