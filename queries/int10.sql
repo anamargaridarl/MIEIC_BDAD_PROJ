a412fa5078bdd4863b89d9be84cdf0fcf459aee0
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-05-26 00:29:51 +0100

    Improved query 10, but it is not finished

--- queries/int10.sql
+++ queries/int10.sql
@@ -3,6 +3,18 @@
 .nullvalue NULL
 
 --  Enfermeiros que trabalharam todos os dias de uma semana
-SELECT idHospital, count(data)
-FROM Funcionario NATURAL JOIN Data NATURAL JOIN Agenda
-GROUP BY idHospital;
+
+-- SELECT DISTINCT data
+-- FROM Data D2
+-- WHERE strftime('%Y-%m-%d',data) >= '2019-04-15' AND strftime('%Y-%m-%d',data) <= '2019-04-21';
+
+-- SELECT *
+-- FROM Funcionario
+-- WHERE idHospital NOT IN (
+                        SELECT *
+                        FROM Data D1
+                        WHERE NOT EXISTS (SELECT DISTINCT data
+                                        FROM (SELECT DISTINCT data
+                                                FROM Data
+                                                WHERE strftime('%Y-%m-%d',data) >= '2019-04-15' AND strftime('%Y-%m-%d',data) <= '2019-04-21') AS D2
+                                        WHERE D1.data<>D2.data);
