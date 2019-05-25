8f5e1ec181b5a251e4e12d380ebf07eae6e51178
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-05-26 00:16:28 +0100

    Changed error message in 3rd trigger to PT

--- triggers/gatilho3_adiciona.sql
+++ triggers/gatilho3_adiciona.sql
@@ -4,5 +4,5 @@ create trigger check_capacity
 before update of capacidade on Departamento
 when ((New.capacidade < Old.capacidade) and (Old.capacidade - Old.vagas > New.capacidade))
 begin
-    select raise(rollback, 'Not enough vacancies!');
+    select raise(rollback, 'Nao ha vagas suficientes!');
 end;
