1335c6eceffefce9b96ed51375efebd0c0fdbff3
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-05-25 17:44:55 +0100

    Added trigger 3 and ability to remove it

--- triggers/gatilho3_adiciona.sql
+++ triggers/gatilho3_adiciona.sql
@@ -1,1 +1,8 @@
 PRAGMA foreign_keys = ON;
+
+create trigger check_capacity
+before update of capacidade on Departamento
+when ((New.capacidade < Old.capacidade) and (Old.capacidade - Old.vagas < New.capacidade))
+begin
+    raise(rollback, "Not enough vacancies!");
+end;
